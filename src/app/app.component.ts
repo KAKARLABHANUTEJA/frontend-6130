8d21b1fb994a12ba924bf16769b4cd820cbb64ef
Bhanu Teja <bhanuteja8339@gmail.com>
2023-01-24 19:02:56 -0500

    removed bootstrap

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Login-page';
+  message = 'Welcome to 6130 Project';
+  members = ['Bhanu','Akhil','Vinay','Sanjay','Kaivalya','Sameeksha','Pavan'];
+}
